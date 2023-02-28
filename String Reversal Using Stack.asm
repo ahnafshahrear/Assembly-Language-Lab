340cde12e0940452df60780b554bb42027a26682
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2023-02-28 12:23:24 +0600

    String Reversal Using Stack added

---
+++ String Reversal Using Stack.asm
@@ -0,0 +1,49 @@
+;AHNAF SHAHREAR KHAN
+;COMPUTER SCIENCE & ENGINEERING
+;UNIVERSITY OF RAJSHAHI
+
+;ASSEMBLY CODE TO READ A STRING & REVERSE IT
+
+.MODEL SMALL
+
+.STACK 100H
+
+.DATA
+
+NEWLINE DB 0AH,0DH,'$'
+
+.CODE
+
+MAIN PROC
+    MOV AX, @DATA
+    MOV DS, AX
+    
+    MOV CX, 0
+    
+    START_INPUT:
+        MOV AH, 1
+        INT 21H
+        CMP AL, 0DH
+        JE END_INPUT
+        PUSH AX
+        INC CX
+        JMP START_INPUT   
+        
+    END_INPUT:
+        
+    MOV AH, 9
+    LEA DX, NEWLINE
+    INT 21H
+    
+    JCXZ EXIT
+    
+    START_OUTPUT:
+         MOV AH, 2
+         POP DX
+         INT 21H
+         LOOP START_OUTPUT
+    
+    EXIT:
+        
+END MAIN
+        
